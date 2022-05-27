b4306fbff44a68c6c709fef67db0ad656493bfa3
ddienu <rockdann@hotmail.com>
2022-05-27 00:45:32 -0500

    Update: Basic page
    Update the basic page with the real code, including a network image and container with text, to give design to the app.

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
 
+final estiloTitulo    = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+final estiloSubTitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
 class BasicPage extends StatelessWidget {
   const BasicPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Design App'),
-      ),
-
+      body: Column(
+        children: [
+          Image(
+            image: NetworkImage('https://www.ettu.org/images/redaktion/News/2021/09_September/big-view_09e98_f_666x375.jpg')
+            ),
+            Container(
+              padding: EdgeInsets.symmetric( horizontal: 30.0, vertical: 20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Mundial de tenis de mesa', style: estiloTitulo,),
+                        SizedBox(height: 7.0),
+                        Text('Escenario principal', style: estiloSubTitulo,),
+                      ],
+                                ),
+                  ),
+              Icon (Icons.star, color: Colors.red, size: 30.0),
+              Text('5', style: TextStyle(fontSize: 20.0),)
+              ],
+              ),
+            )
+        ],
+      )
     );
   }
 }
