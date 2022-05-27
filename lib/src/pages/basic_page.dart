f35ed0e73b00423d78fe8ab76c3bc4db716832c5
ddienu <rockdann@hotmail.com>
2022-05-26 22:50:18 -0500

    Update: Main, add: basic page
    Create design project, updating the main page to get routes to show the page.
    Create basic page with a test code and from it start the real coding.

---
+++ lib/src/pages/basic_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BasicPage extends StatelessWidget {
+  const BasicPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Design App'),
+      ),
+
+    );
+  }
+}
