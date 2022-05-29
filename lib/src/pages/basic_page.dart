2f97f026b97d96e4e042716e7b86013f9c296363
ddienu <rockdann@hotmail.com>
2022-05-28 23:56:01 -0500

    Update: Basic page
    Update the basic page, adding the icons to the page with its name respectly and a text wrapping in a container to organize the text in a better way.

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -12,10 +12,23 @@ class BasicPage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Image(
+          _crearImagen(),
+          _crearTitulo(),     
+          _crearAcciones(),      
+          _crearTexto(), 
+        ],
+      )
+    );
+  }
+ Widget _crearImagen(){
+    return Image(
             image: NetworkImage('https://www.ettu.org/images/redaktion/News/2021/09_September/big-view_09e98_f_666x375.jpg')
-            ),
-            Container(
+            );
+  }
+ Widget _crearTitulo(){
+
+
+    return Container(
               padding: EdgeInsets.symmetric( horizontal: 30.0, vertical: 20.0),
               child: Row(
                 children: [
@@ -32,10 +45,41 @@ class BasicPage extends StatelessWidget {
               Icon (Icons.star, color: Colors.red, size: 30.0),
               Text('5', style: TextStyle(fontSize: 20.0),)
               ],
-              ),
-            )
-        ],
-      )
+             ),
+            );
+  }
+
+  Widget _crearAcciones(){
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'SHARE'),
+      ],
+    );
+
+  }
+  Widget _accion(IconData icon, String texto){
+      return Column(
+          children: [
+
+            Icon( icon, color: Colors.blue, size: 40.0, ),
+            SizedBox( height: 5.0,),
+            Text( texto, style: TextStyle(fontSize: 15, color: Colors.blue, fontWeight: FontWeight.bold)),
+          ],
+        );
+  }
+  
+  Widget _crearTexto() {
+
+    return Container(
+      padding: EdgeInsets.symmetric( horizontal: 40.0, vertical: 20.0),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.',
+        textAlign: TextAlign.justify,
+         ),
     );
   }
 }
