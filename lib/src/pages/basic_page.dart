59528fc6bbd472de3b0d92611a8c4abd9ba99357
ddienu <rockdann@hotmail.com>
2022-05-29 00:32:27 -0500

    Update: Basic Page
    Visual upgrades.

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -10,43 +10,52 @@ class BasicPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
           _crearImagen(),
           _crearTitulo(),     
           _crearAcciones(),      
           _crearTexto(), 
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
         ],
+        ),
+        
       )
     );
   }
  Widget _crearImagen(){
     return Image(
-            image: NetworkImage('https://www.ettu.org/images/redaktion/News/2021/09_September/big-view_09e98_f_666x375.jpg')
+            image: NetworkImage('https://www.ettu.org/images/redaktion/News/2021/09_September/big-view_09e98_f_666x375.jpg'),
+            fit: BoxFit.cover,
             );
   }
  Widget _crearTitulo(){
 
 
-    return Container(
-              padding: EdgeInsets.symmetric( horizontal: 30.0, vertical: 20.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Mundial de tenis de mesa', style: estiloTitulo,),
-                        SizedBox(height: 7.0),
-                        Text('Escenario principal', style: estiloSubTitulo,),
-                      ],
-                                ),
-                  ),
-              Icon (Icons.star, color: Colors.red, size: 30.0),
-              Text('5', style: TextStyle(fontSize: 20.0),)
-              ],
-             ),
-            );
+    return SafeArea(
+      child: Container(
+                padding: EdgeInsets.symmetric( horizontal: 30.0, vertical: 20.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Mundial de tenis de mesa', style: estiloTitulo,),
+                          SizedBox(height: 7.0),
+                          Text('Escenario principal', style: estiloSubTitulo,),
+                        ],
+                                  ),
+                    ),
+                Icon (Icons.star, color: Colors.red, size: 30.0),
+                Text('5', style: TextStyle(fontSize: 20.0),)
+                ],
+               ),
+              ),
+    );
   }
 
   Widget _crearAcciones(){
@@ -74,12 +83,14 @@ class BasicPage extends StatelessWidget {
   
   Widget _crearTexto() {
 
-    return Container(
-      padding: EdgeInsets.symmetric( horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.',
-        textAlign: TextAlign.justify,
-         ),
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric( horizontal: 40.0),
+        child: Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.',
+          textAlign: TextAlign.justify,
+           ),
+      ),
     );
   }
 }
