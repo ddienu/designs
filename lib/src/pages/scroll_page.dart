5738207caccd628615a7c07b2fb7a997ebff7f1b
ddienu <rockdann@hotmail.com>
2022-05-30 01:58:03 -0500

    Update: Scroll Page
    Update the scroll page adding the widgets and adding some methods to compile the code better.

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 
 class ScrollPage extends StatelessWidget {
-  const ScrollPage({Key? key}) : super(key: key);
+  ScrollPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +18,60 @@ class ScrollPage extends StatelessWidget {
   }
   
   Widget _pagina1() {
-    return Center(
-       child: Text('Pagina 1'),
+    return Stack(
+      children: [
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
     );
+
+
+
+
+
+    
   }
   Widget _pagina2() {
     return Center(
        child: Text('Pagina 2'),
+    );
+  }
+  
+  Widget _colorFondo() {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+  
+   Widget _imagenFondo() {
+     return Container(
+       height: double.infinity,
+       width: double.infinity,
+       child: Image(
+         image: AssetImage('assets/9.1 scroll-1.png'),
+         fit: BoxFit.cover,
+         ),
+         
+     );
+   }
+   
+
+   final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+  Widget _textos() {
+    return  SafeArea(
+      child: Column(
+          children: [
+            SizedBox(height: 50.0,),
+            Text('11°', style: estiloTexto,),
+            Text('Miércoles', style: estiloTexto,),
+            Expanded(child: Container()),
+            Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+          ],
+        ),
     );
   }
 }
